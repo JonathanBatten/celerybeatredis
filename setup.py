85980c58dc20a0d85833f450bd2c4f145df164db
Jonathan Batten <jjbatten@gmail.com>
2018-08-31 15:29:09 +1000

    Restrict `celery` package version less than `4.0`

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name = "celerybeat-redis",
     description = "A Celery Beat Scheduler that uses Redis to store both schedule definitions and status information",
-    version = "0.1.5",
+    version = "0.1.6",
     license = "Apache License, Version 2.0",
     author = "Kong Luoxing",
     author_email = "kong.luoxing@gmail.com",
@@ -20,7 +20,7 @@ setup(
     install_requires=[
         'setuptools',
         'redis>=2.10.3',
-        'celery>=3.1.16'
+        'celery>=3.1.16,<4.0'
     ]
 
 )
